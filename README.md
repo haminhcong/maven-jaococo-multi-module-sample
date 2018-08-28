4deb370c1d5418c0e3ad23565253ffc72d33de36
haminhcong <haminhcongbkhn@gmail.com>
2018-08-29 01:37:41 +0700

    Initial commit Add test Add doc

---
+++ README.md
@@ -0,0 +1,11 @@
+# maven-jaococo-multi-module-sample
+
+Sample demonstrate how jacoco work with maven multi module
+
+Aggregate result in report/target/jacoco-aggregate-customization folder 
+
+To run and see coverage result, run:
+
+```bash
+mvn clean compile test verify
+```
---
+++ child2/src/main/java/package2/Example2.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2018 Mountainminds GmbH & Co. KG and Contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marc R. Hoffmann, Jan Wloka - initial API and implementation
+ *
+ *******************************************************************************/
+package package2;
+
+public class Example2 {
+
+  public void a() {
+    System.out.println("A");
+  }
+  
+  public void b() {
+    System.out.println("B");
+  }  
+  
+  public void c() {
+    System.out.println("C");
+  }
+}
